8b58f7ddab439c5b39c3675556dcd03020454706
vita.minka <roachggwp@gmail.com>
2025-08-26 15:42:46 +1000

    create script for zabbix discovery

---
+++ zabbix_discover_nets.py
@@ -0,0 +1,19 @@
+#!./venv/bin/python3
+import os
+import json
+
+self_dir = os.getcwd() + '/'
+config_dir = self_dir
+config_file = config_dir + 'config.json'
+zabbix_data = {"data": []}
+
+with open(config_file, 'r') as f:
+    config = json.load(f)
+
+for i in range(len(config)):
+    net = config["nets"][i]
+    ssid = net["ssid"]
+    net_vars = {"#ID": i, "#SSID": ssid}
+    zabbix_data["data"].append(net_vars)
+
+print(json.dumps(zabbix_data))
