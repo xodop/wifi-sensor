baf2fae26fef94a433023efbfe9427d831e9ed0b
vita.minka <roachggwp@gmail.com>
2025-11-06 17:34:16 +1000

    script version 2: separate independent output data by connections, by bssids and by channels

--- main.py
+++ main.py
@@ -65,6 +65,41 @@ def test_connection(interface, ssid, config_file, timeout=5):
                     result[line[0].strip()] = line[1].strip().split()[0]    #rate in Mbit/s
     return result
 
+def search_aps_by_ssid(interface, ssid, file_name, timeout=60):
+
+    result = {}
+    data_file = file_name + '-01.kismet.csv'
+    key_filter = ['NetType', 'BSSID', 'ESSID', 'Channel', 'Beacon', 'Data', 'Total', 'BestQuality', 'BestSignal', 'BestNoise', 'MaxRate', 'MaxSeenRate', 'Encryption', 'FirstTime', 'LastTime', 'Carrier']
+    update_wlan_type(interface, 'monitor')
+
+    airodump_ng_proc = subprocess.Popen(
+            ['airodump-ng', f'{interface}', '--ignore-negative-one', '--output-format', 'kismet', '-n', '10', '-N', f'{ssid}', '-w', f'{file_name}'],
+            stdout=subprocess.DEVNULL,
+            stderr=subprocess.DEVNULL,
+            encoding='utf-8'
+    )
+    time.sleep(timeout/2)
+    airodump_ng_proc.kill()
+
+    for item in parse_csv(data_file, key_filter, sep=";"):
+        result[item.pop("BSSID")] = item
+    os.remove(data_file)
+    time.sleep(1)
+    airodump_ng_proc = subprocess.Popen(
+            ['airodump-ng', f'{interface}', '--ignore-negative-one', '--output-format', 'kismet', '-n', '10', '-N', f'{ssid}', '-w', f'{file_name}', '-b', 'a'],
+            stdout=subprocess.DEVNULL,
+            stderr=subprocess.DEVNULL,
+            encoding='utf-8'
+    )
+    time.sleep(timeout/2)
+    airodump_ng_proc.kill()
+
+    for item in parse_csv(data_file, key_filter, sep=";"):
+        result[item.pop("BSSID")] = item
+    os.remove(data_file)
+
+    return result
+
 
 def test_channel(interface, channel, file_name, timeout=60):
     update_wlan_type(interface, 'monitor')
@@ -136,6 +171,53 @@ def convert_to_seconds(time_str):
         raise ValueError(f'Unknown unit: {unit}')
     return number * multipliers[unit]
 
+def wifi_channel_to_freq(channel):
+    #dict of wifi channel numbers and related frequencies in MHz
+    wifi_freqs = {
+        '1': '2412',
+        '2': '2417',
+        '3': '2422',
+        '4': '2427',
+        '5': '2432',
+        '6': '2437',
+        '7': '2442',
+        '8': '2447',
+        '9': '2452',
+        '10': '2457',
+        '11': '2462',
+        '12': '2467',
+        '13': '2472',
+        '14': '2484',
+        '36': '5180',
+        '40': '5200',
+        '44': '5220',
+        '48': '5240',
+        '52': '5260',
+        '56': '5280',
+        '60': '5300',
+        '64': '5320',
+        '100': '5500',
+        '104': '5520',
+        '108': '5540',
+        '112': '5560',
+        '116': '5580',
+        '120': '5600',
+        '124': '5620',
+        '128': '5640',
+        '132': '5660',
+        '136': '5680',
+        '140': '5700',
+        '144': '5720',
+        '149': '5745',
+        '153': '5765',
+        '157': '5785',
+        '161': '5805',
+        '165': '5825'
+    }
+    if type(channel) == int:
+        channel = str(channel)
+    return wifi_freqs[channel]
+
 
 def wifi_freq_to_channel(f_mhz):
     #dict of wifi frequencies in MHz and related channel numbers
@@ -180,6 +262,8 @@ def wifi_freq_to_channel(f_mhz):
         '5805': '161',
         '5825': '165'
     }
+    if type(f_mhz) == int:
+        channel = str(channel)
     return wifi_channels[f_mhz]
 
 
@@ -202,7 +286,12 @@ if __name__ == "__main__":
     config_dir = self_dir
     config_file = config_dir + 'config.json'
     tmp_dir = '/tmp/wifi-sensor/'
-    dict_of_results = {}
+    seen_channels = set()
+    dict_of_results = {
+        'connections': {},
+        'seen_aps': {},
+        'seen_channels': {}
+    }
     result_file = '/tmp/result.json'
     retry_limit = 2
     env = Environment(
@@ -237,90 +326,96 @@ if __name__ == "__main__":
         #retry if connection fails
         retry_count = 0 
         while result['status'] != '0' and retry_count <= retry_limit:
-            result = test_connection(mon_if, net['ssid'], tmp_cfg_file, timeout=30)
+            result = test_connection(mon_if, net['ssid'], tmp_cfg_file, timeout=10)
             retry_count += 1
-
-        #print(result)
-        #test wifi channel with airodump-ng
-        if result['status'] == '0':
-
-            result['channel'] = wifi_freq_to_channel(result['freq']) 
-            result['cci_ap_list'] = []
-
-            data_file_noext = tmp_dir + 'capture_' + result['channel'] 
-            test_channel(mon_if, result['channel'], data_file_noext, timeout=30)
-    
-            #filter kismet output and convert csv to json
-            src_data_file = data_file_noext + '-01.kismet.csv'
-            key_filter = ['Network', 'NetType', 'BSSID', 'ESSID', 'Channel', 'Beacon', 'Data', 'Total', 'BestQuality', 'BestSignal', 'BestNoise', 'MaxRate', 'MaxSeenRate', 'Encryption', 'FirstTime', 'LastTime', 'Carrier'] 
-
-            channel_scan = parse_csv(src_data_file, sep=";")
-            os.remove(src_data_file)
-            
-            #get stations on channel from csv
-            src_data_file = data_file_noext +  '-01.csv'
-            with open(src_data_file, 'r+') as f:
-                channel_stations = f.read().split('\n\n')[1]
-                f.seek(0)
-                f.write(channel_stations)
-            channel_stations = parse_csv(src_data_file, sep=",")
-            os.remove(src_data_file)
-
-            #pprint(channel_scan)
-            for ap in channel_scan:
-                if ap['ESSID'] == net['ssid'] and ap['BSSID'] == result['bssid']:
-                    result['beacon'] = ap['Beacon']
-                    result['data'] = ap['Data']
-                    result['encryption'] = ap['Encryption']
-                    result['rate'] = ap['MaxSeenRate']
-                    result['noise'] = ap['BestNoise']
-            counter = 0
-            threshold = 70    #module of threshold for signal strength in dBm
-            conn_bssid = result['bssid']
-            conn_bssid_nic = conn_bssid[len(conn_bssid)//2+1:] # last 6 octets for NIC
-            for ap in channel_scan:
-                ap_bssid = ap['BSSID']
-                ap_bssid_nic = ap_bssid[len(ap_bssid)//2+1:] #last 6 octetc for NIC
-                if ap_bssid_nic != conn_bssid_nic and int(ap['BestQuality']) > -threshold and ap['BestQuality'] != '-1':
-                    counter += 1
-                    result['cci_ap_list'].append({ 'bssid': ap['BSSID'], 'ssid': ap['ESSID'], 'signal': ap['BestQuality'], 'channel': ap['Channel'] })
-            result['cci_aps'] = str(counter)
-
-            #pprint(channel_stations)
-            counter = 0
-            counter_threshold = 0 
-            for station in channel_stations:
-                counter += 1
-                try:
-                    if int(station[' Power']) > -threshold:
-                        counter_threshold += 1
-                except:
-                    pass
-            result['stations'] = str(counter)
-            result[f'stations_{threshold}dbm'] = str(counter_threshold)
-
-            #get channel airtime data
-            survey = test_channel_airtime(mon_if, result['freq'])
-            survey = survey.split('\n')
-            for line in survey:
-                line = line.split(':')
-                if line[0].strip() != 'frequency':
-                    if 'time' in line[0].strip():
-                        result[line[0].strip()] = str('{:.6f}'.format(round(convert_to_seconds(line[1].strip()), 6)))
-                    else:
-                        result[line[0].strip()] = line[1].strip()
-            
-            #rename keys which contain whitespaces
-            result['rx_bitrate'] = result.pop('rx bitrate') 
-            result['tx_bitrate'] = result.pop('tx bitrate')
-            result['active_time'] = result.pop('channel active time')
-            result['busy_time'] = result.pop('channel busy time')
-            result['transmit_time'] = result.pop('channel transmit time')
-        
         result['conn_retries'] = str(retry_count)
+        result['rx_bitrate'] = result.pop('rx bitrate')
+        result['tx_bitrate'] = result.pop('tx bitrate')
+
+        #append connection result to enumerated dict of connection results    
+        dict_of_results['connections'][i] = result
+        #print(result)
+
+        #search all active BSSIDs for this net
+        data_file_noext = tmp_dir + 'channels_on_' + net['ssid'] 
+        seen_aps = search_aps_by_ssid(mon_if, net['ssid'], data_file_noext, timeout=30)
         
-        #append result to enumerated dict of results    
-        dict_of_results[i] = result
+        #print(seen_aps)
+        
+        for k,v in seen_aps.items():
+            if k and k != 0 and k != '0':
+                dict_of_results['seen_aps'][k] = v
+                #fill list of seen channels
+                seen_channels.add(v['Channel'])
+
+    #print(seen_channels)    
+
+    #test wifi channels with airodump-ng
+    for channel in seen_channels:
+        result = {}
+        result['cci_ap_list'] = []
+
+        data_file_noext = tmp_dir + 'capture_' + channel
+        test_channel(mon_if, channel, data_file_noext, timeout=30)
+    
+        #filter kismet output and convert csv to json
+        src_data_file = data_file_noext + '-01.kismet.csv'
+        key_filter = ['NetType', 'BSSID', 'ESSID', 'Channel', 'Beacon', 'Data', 'Total', 'BestQuality', 'BestSignal', 'BestNoise', 'MaxRate', 'MaxSeenRate', 'Encryption', 'FirstTime', 'LastTime', 'Carrier'] 
+        channel_scan = parse_csv(src_data_file, sep=";")
+        os.remove(src_data_file)
+            
+        #get stations on channel from csv
+        src_data_file = data_file_noext +  '-01.csv'
+        with open(src_data_file, 'r+') as f:
+            channel_stations = f.read().split('\n\n')[1]
+            f.seek(0)
+            f.write(channel_stations)
+        channel_stations = parse_csv(src_data_file, sep=",")
+        os.remove(src_data_file)
+
+        counter = 0
+        threshold = 70    #module of threshold for signal strength in dBm
+        for ap in channel_scan:
+            ap_bssid = ap['BSSID']
+            ap_bssid_nic = ap_bssid[len(ap_bssid)//2+1:] #last 6 octetc for NIC
+            if int(ap['BestQuality']) > -threshold and ap['BestQuality'] != '-1':
+                counter += 1
+                result['cci_ap_list'].append({ 'bssid': ap['BSSID'], 'ssid': ap['ESSID'], 'signal': ap['BestQuality'], 'channel': ap['Channel'] })
+        result['cci_aps'] = str(counter)
+
+        #pprint(channel_stations)
+        counter = 0
+        counter_threshold = 0 
+        for station in channel_stations:
+            counter += 1
+            try:
+                if int(station[' Power']) > -threshold:
+                    counter_threshold += 1
+            except:
+                pass
+        result['stations'] = str(counter)
+        result[f'stations_{threshold}dbm'] = str(counter_threshold)
+
+        #get channel airtime data
+        freq = wifi_channel_to_freq(channel)
+        result['freq'] = freq
+        survey = test_channel_airtime(mon_if, freq)
+        survey = survey.split('\n')
+        for line in survey:
+            line = line.split(':')
+            if line[0].strip() != 'frequency':
+                if 'time' in line[0].strip():
+                    result[line[0].strip()] = str('{:.6f}'.format(round(convert_to_seconds(line[1].strip()), 6)))
+                else:
+                    result[line[0].strip()] = line[1].strip()
+            
+        #rename keys which contain whitespaces
+        result['active_time'] = result.pop('channel active time')
+        result['busy_time'] = result.pop('channel busy time')
+        result['transmit_time'] = result.pop('channel transmit time')
+        
+        #append channel scan result to dict of channel scan results 
+        dict_of_results['seen_channels'][channel] = result
 
     #pprint(dict_of_results)    
     #write json to result file and delete tmp dir
