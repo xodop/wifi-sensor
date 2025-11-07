1c1b56853e67afa6ac1a9616e93df1be7ae7e16f
vita.minka <roachggwp@gmail.com>
2025-11-07 11:07:02 +1000

    script version 2: rework discovery

--- zabbix_discover_nets.py
+++ zabbix_discover_nets.py
@@ -1,19 +1,30 @@
 #!./venv/bin/python3
 import os
+from sys import argv
 import json
 
-self_dir = os.getcwd() + '/'
-config_dir = self_dir
-config_file = config_dir + 'config.json'
-zabbix_data = {"data": []}
+result_file = '/tmp/result.json'
+zabbix_data = {'data': []}
 
-with open(config_file, 'r') as f:
-    config = json.load(f)
+with open(result_file, 'r') as f:
+    result = json.load(f)
 
-for i in range(len(config["nets"])):
-    net = config["nets"][i]
-    ssid = net["ssid"]
-    net_vars = {"{#NUM}": i, "{#SSID}": ssid}
-    zabbix_data["data"].append(net_vars)
+if len(argv) == 1 or argv == '--connection':
+    for i in list(result['connections'].keys()):
+        i_vars = {'{#NUM}': i, '{#SSID}': result['connections'][i]['ssid']}
+        zabbix_data["data"].append(i_vars)
+elif len(argv) > 2:
+    raise ValueError('Wrong number of arguments! Available one argument at once')
+elif argv[1] == '--ap':
+    for i in list(result['seen_aps'].keys()):
+        i_vars = {"{#BSSID}": i}
+        zabbix_data["data"].append(i_vars) 
+elif argv[1] == '--channel':
+    for i in list(result['seen_channels'].keys()):
+        i_vars = {"{#CHANNEL}": i}
+        zabbix_data["data"].append(i_vars)
+else:
+    raise ValueError('Wrong argument! Available agruments: --connection (default), --ap or --channel')
 
 print(json.dumps(zabbix_data))
+
