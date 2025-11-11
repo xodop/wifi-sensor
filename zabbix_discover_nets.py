1c003049971b2bb8fcd60d3afd82eb156eb00f12
vita.minka <roachggwp@gmail.com>
2025-11-11 18:02:45 +1000

    fix bug with zabbix discovery script

--- zabbix_discover_nets.py
+++ zabbix_discover_nets.py
@@ -11,17 +11,17 @@ with open(result_file, 'r') as f:
 
 if len(argv) == 1 or argv[1] == '--connection':
     for k, v in result['connections'].items():
-        i_vars = {'{#SSID}': v['ssid']}
+        i_vars = {'{#SSID}': k}
         zabbix_data["data"].append(i_vars)
 elif len(argv) > 2:
     raise ValueError('Wrong number of arguments! Available one argument at once')
 elif argv[1] == '--ap':
     for k,v in result['seen_aps'].items():
-        i_vars = {'{#BSSID}': v['BSSID'], '{#SSID}': v['ESSID'], '{#CH}': v['Channel']}
+        i_vars = {'{#BSSID}': k, '{#SSID}': v['ESSID'], '{#CH}': v['Channel']}
         zabbix_data["data"].append(i_vars) 
 elif argv[1] == '--channel':
     for k,v in result['seen_channels'].items():
-        i_vars = {'{#CH}': v['channel']}
+        i_vars = {'{#CH}': k}
         zabbix_data["data"].append(i_vars)
 else:
     raise ValueError('Wrong argument! Available agruments: --connection (default), --ap or --channel')
